7a6b2ad523c4ebe3a78b6ec52c223cad1ae4d5f5
Marco Corapi <marco.corapi@outlook.it>
2021-10-15 09:39:10 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+interface IState {
+  //array of objects
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string // optional, either a string or undefined: hover it
+  }[]
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test App</h1>
     </div>
   );
 }
